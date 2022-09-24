437f6ea0696699d86384b08e173efb4dd4bbf086
Mohammed Nayeem <chiefnayeem@gmail.com>
2022-09-24 21:14:16 +0600

    Updated marker css

--- js/PolygonEditor.js
+++ js/PolygonEditor.js
@@ -626,10 +626,10 @@ class PolygonEditor extends PolygonInstance {
         document.querySelector('svg').innerHTML =
           document.querySelector('svg').innerHTML +
           `
-          <g id="Vivid.JS" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" style="translate: ${d3?.event?.offsetX - 8}px ${d3?.event?.offsetY - 25}px; transform: scale(0.6);">
-            <g id="Vivid-Icons" transform="translate(-125.000000, -643.000000)">
-              <g id="Icons" transform="translate(37.000000, 169.000000)">
-                <g id="map-marker" transform="translate(78.000000, 468.000000)">
+          <g class="marker-point" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" style="translate: ${d3?.event?.offsetX - 8}px ${d3?.event?.offsetY - 25}px; transform: scale(0.6);">
+            <g transform="translate(-125.000000, -643.000000)">
+              <g transform="translate(37.000000, 169.000000)">
+                <g transform="translate(78.000000, 468.000000)">
                   <g transform="translate(10.000000, 6.000000)">
                     <path d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z" id="Shape" fill="#ffffff"></path>
                     <circle id="Oval" fill="#ffffff" fill-rule="nonzero" cx="14" cy="14" r="7"></circle>
